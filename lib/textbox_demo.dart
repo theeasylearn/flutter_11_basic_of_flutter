890ccc75ee7ff3199acddf76dc1a4009a61d7ea5
theeasylearn <theeasylearn@gmail.com>
2025-11-18 18:28:32 +0530

    Initial commit

--- lib/textbox_demo.dart
+++ lib/textbox_demo.dart
@@ -7,6 +7,41 @@ class TextboxDemo extends StatefulWidget {
 }
 
 class _TextboxDemoState extends State<TextboxDemo> {
+  //create 3 TextEditingController type object
+  TextEditingController amountController = new TextEditingController();
+  TextEditingController rateController = new TextEditingController();
+  TextEditingController yearController = new TextEditingController();
+
+  //create 3 String type variable
+  double amount = 0.0, rate = 0.0, year = 0.0;
+  int count = 0;
+  String result = '';
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    count = count + 1;
+    print("init state method called $count");
+    //bind TextEditingController with TextInputField
+    amountController.addListener(() {
+        if(amountController.text !=null && amountController.text.trim() != '')
+        {
+            amount = double.parse(amountController.text);
+        }
+    });
+    rateController.addListener(() {
+        if(rateController.text != null && rateController.text.trim() != '')
+        {
+          rate = double.parse(rateController.text);
+        }
+    });
+    yearController.addListener(() {
+      if(yearController.text != null && yearController.text.trim() != '')
+      {
+        year = double.parse(yearController.text);
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,7 +61,7 @@ class _TextboxDemoState extends State<TextboxDemo> {
           ),
         body: Center(
             child: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.50,
+              height: MediaQuery.of(context).size.height * 0.60,
               child: Padding(
                 padding: EdgeInsets.only(left: 20,right: 20),
                 child: Card(
@@ -37,6 +72,7 @@ class _TextboxDemoState extends State<TextboxDemo> {
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         TextFormField(
+                          controller: amountController,
                           keyboardType: TextInputType.number,
                           decoration: InputDecoration(
                             hintText: "Enter Amount",
@@ -44,6 +80,7 @@ class _TextboxDemoState extends State<TextboxDemo> {
                         ),
                         SizedBox(height: 40,),
                         TextFormField(
+                          controller: rateController,
                           keyboardType: TextInputType.number,
                           decoration: InputDecoration(
                             hintText: "Rate"
@@ -51,6 +88,7 @@ class _TextboxDemoState extends State<TextboxDemo> {
                         ),
                         SizedBox(height: 40,),
                         TextFormField(
+                          controller: yearController,
                           keyboardType: TextInputType.number,
                           decoration: InputDecoration(
                               hintText: "Year"
@@ -61,9 +99,18 @@ class _TextboxDemoState extends State<TextboxDemo> {
                             color: Colors.brown,
                             textColor: Colors.white,
                             onPressed: () {
+                              //calculate interest
+                              double interest = (amount * rate * year) / 100;
+                              this.setState(() {
+                                  result = "simple interest = $interest";
+                              });
                         }, child: Text("Calculate interest", style: TextStyle(
                           fontSize: 20
                         ),)),
+                        SizedBox(height: 40,),
+                        Text(result,style: TextStyle(
+                          fontSize: 20
+                        ),)
                       ],
                     ),),
                 ),
